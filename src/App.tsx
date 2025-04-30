07f41519b38f2ec8ee76996fa1e09ec13ed1f634
BoGoBooM <myPikcha@gmail.com>
2025-04-30 17:08:48 +0300

    task solution

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export class App extends React.Component<{}, State> {
     pressedKey: null,
   };
 
-  handleKeyUp = (event: KeyboardEvent) => {
+  handleKeyUp = (event: React.KeyboardEvent<HTMLDivElement>) => {
     this.setState({ pressedKey: event.key });
   };
 
